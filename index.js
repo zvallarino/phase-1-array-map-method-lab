bc3c046b56c23d12bb26b19b083cd3eef4430923
Zachary <zvcoding@gmail.com>
2021-09-26 17:06:04 -0400

    commit tree 8d9fee689a7e7490c9d40faa81e7101365587baa

--- index.js
+++ index.js
@@ -11,6 +11,32 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const testWords =['what', 'does', 'the', 'this', 'keyword', 'mean?'];
+const testWord = `sketch`
+const arrayTest =[2,3,4,5]
+
+capital = (word) => {
+  return word[0].toUpperCase() + word.slice(1);
 }
+
+// console.log(capital(testWord));
+
+outterFunction = (array) => {array.map((element)=>console.log(element))}
+
+innerFunction = (array) => {
+  let map1 = array.map((element)=>{
+    return (capital(element))})
+  return map1.join(" "); 
+}
+
+const titleCased = (array) => {
+  
+  let map2 = array.map((element) => {
+  let newWords = element.split(" ");
+  return innerFunction(newWords);
+})
+  return map2}
+
+// outterFunction(tutorials);
+// console.log(innerFunction(testWords));
+console.log(titleCased(tutorials));
